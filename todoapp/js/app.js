f4c6db1902ff5c7f68a4b29199735ca2f7be919e
kimura511 <DEN03471@nifty.com>
2016-05-13 09:26:36 +0000

    finishing step6

--- todoapp/js/app.js
+++ todoapp/js/app.js
@@ -1,5 +1,5 @@
 angular.module('App', ['LocationBar'])
-.controller('MainController', ['$scope', function ($scope) {
+.controller('MainController', ['$scope', '$filter', function ($scope, $filter) {
   $scope.todos = [];
 
   $scope.newTitle = '';
@@ -24,4 +24,19 @@ angular.module('App', ['LocationBar'])
   $scope.changeFilter = function (filter) {
     $scope.currentFilter = filter;
   };
+  
+  $scope.$watch('todos', function (todos) {
+  // todos が増減したり各要素のプロパティが変更された時に実行される
+  }, true);
+  
+   var where = $filter('filter'); // filter フィルタ関数の取得
+  $scope.$watch('todos', function (todos) {
+    var length = todos.length;
+
+    $scope.allCount = length;                                   // 総件数モデル
+    $scope.doneCount = where(todos, $scope.filter.done).length; // 完了件数モデル
+    $scope.remainingCount = length - $scope.doneCount;          // 未了件数モデル
+  }, true);
 }]);
+
+
