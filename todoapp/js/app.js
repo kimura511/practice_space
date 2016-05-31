1cea4c7a34c806c3f19ad2e836583a701794a4fb
kimura511 <DEN03471@nifty.com>
2016-05-31 03:13:12 +0000

    tetris sample

--- todoapp/js/app.js
+++ todoapp/js/app.js
@@ -1,5 +1,19 @@
 angular.module('App', ['LocationBar'])
 .controller('MainController', ['$scope', '$filter', function ($scope, $filter) {
+    .directive('mySelect', [function () {
+    return function (scope, $el, attrs) {
+      // scope - 現在の $scope オブジェクト
+      // $el   - jqLite オブジェクト(jQuery ライクオブジェクト)
+      //         jQuery 使用時なら jQuery オブジェクト
+      // attrs - DOM 属性のハッシュ(属性名は正規化されている)
+  
+      scope.$watch(attrs.mySelect, function (val) {
+        if (val) {
+          $el[0].select();
+        }
+      });
+    };
+  }]);
   $scope.todos = [];
 
   $scope.newTitle = '';
@@ -37,6 +51,22 @@ angular.module('App', ['LocationBar'])
     $scope.doneCount = where(todos, $scope.filter.done).length; // 完了件数モデル
     $scope.remainingCount = length - $scope.doneCount;          // 未了件数モデル
   }, true);
+  var originalTitle;     // 編集前の要件
+  $scope.editing = null; // 編集モードの ToDo モデルを表すモデル
+  $scope.editTodo = function (todo) {
+  originalTitle = todo.title;
+  $scope.editing = todo;
+  };
+  $scope.doneEdit = function () {
+  $scope.editing = null;
+  };
+  
+  var originalTitle;     // 編集前の要件
+  $scope.editing = null; // 編集モードの ToDo モデルを表すモデル
+  $scope.editTodo = function (todo) {
+  originalTitle = todo.title;
+  $scope.editing = todo;
+};
 }]);
 
 
